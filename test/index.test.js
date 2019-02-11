94c8300a48ff1d28b27ed94cfaa2040f597715cc
Benny Megidish <bennymegk@gmail.com>
2019-02-11 17:16:36 +0200

    added tests

---
+++ test/index.test.js
@@ -0,0 +1,42 @@
+const ReadmeSummarizer = require('../index').ReadmeSummarizer;
+const testResources = require('./resources');
+const assert = require('chai').assert;
+
+describe('Readme Summarizer', () => {
+    let expectedShortDescription = testResources.expectedShortDescription;
+    let expectedLongDescription = testResources.expectedLongDescription;
+
+    describe('From text', () => {
+        let readme = testResources.readme;
+
+        it('should summarize readme to a long description from text', () => {
+            let description = ReadmeSummarizer.fromMarkdownText(readme);
+            assert.equal(description, expectedLongDescription);
+        });
+
+        it('should summarize readme to a short description from text', () => {
+            let description = ReadmeSummarizer.fromMarkdownText(readme, true);
+            assert.equal(description, expectedShortDescription);
+        });
+    });
+
+    describe('From URL', () => {
+        let url = testResources.readmeUrl;
+
+        it('should summarize readme to a long description from url', () => {
+            ReadmeSummarizer.fromUrl(url).then((result) => {
+                assert.equal(result, expectedLongDescription);
+            }).catch(() => {
+                assert.fail();
+            });
+        });
+
+        it('should summarize readme to a short description from url', () => {
+            ReadmeSummarizer.fromUrl(url, true).then((result) => {
+                assert.equal(result, expectedShortDescription);
+            }).catch(() => {
+                assert.fail();
+            });
+        });
+    });
+});
---
+++ test/test-resources.js
@@ -0,0 +1,24 @@
+const readmeUrl = 'https://raw.githubusercontent.com/bennymeg/IsraelPostalServiceAPI/master/README.md';
+
+const expectedShortDescription = "An API for Israel postal service - query shipment price easily.";
+
+const expectedLongDescription = "An API for Israel postal service - query shipment price easily.\n" +
+    "Supports both Node and browser application bases on browserify / webpack (including Angular).";
+
+const readme = `# Israel Postal Service API
+An API for Israel postal service - query shipment price easily.
+Supports both Node and browser application bases on browserify / webpack (including Angular).
+
+[![licence](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/bennymeg/IsraelPostalServiceAPI/blob/master/LICENSE)
+[![npm version](https://img.shields.io/npm/v/israel-postal-service-api.svg)](https://www.npmjs.com/package/israel-postal-service-api)
+<!-- [![github version](https://img.shields.io/github/package-json/v/badges/shields.svg)](https://github.com/bennymeg/IsraelPostalServiceAPI) -->
+<!-- ![GitHub repository size in bytes](https://img.shields.io/github/languages/code-size/badges/shields.svg) -->
+
+
+## Installation
+install`;
+
+module.exports.expectedShortDescription = expectedShortDescription;
+module.exports.expectedLongDescription = expectedLongDescription;
+module.exports.readme = readme;
+module.exports.readmeUrl = readmeUrl;
