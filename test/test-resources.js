5b8eabf4a269a09f109a6c25acc0f075241f84f5
Benny Megidish <bennymegk@gmail.com>
2019-02-12 14:34:28 +0200

    more test options

--- test/test-resources.js
+++ test/test-resources.js
@@ -2,9 +2,12 @@ const readmeUrl = 'https://raw.githubusercontent.com/bennymeg/IsraelPostalServic
 const readmeUrlTest ='https://raw.githubusercontent.com/bennymeg/ReadmeSummarizer/master/README.md';
 
 const expectedShortDescription = "An API for Israel postal service - query shipment price easily.";
+const expectedShortTestDescription = "Summaries markdown readme files into few sentences.";
 
 const expectedLongDescription = "An API for Israel postal service - query shipment price easily.\n" +
     "Supports both Node and browser application bases on browserify / webpack (including Angular).";
+const expectedLongTestDescription = "Summaries markdown readme files into few sentences. " +
+    "Get short description for any repository easily.";
 
 const readme = `# Israel Postal Service API
 An API for Israel postal service - query shipment price easily.
@@ -17,9 +20,23 @@ Supports both Node and browser application bases on browserify / webpack (includ
 
 
 ## Installation
-install`;
+install...`;
+
+const readmeTest = `# Readme Summarizer
+Summaries markdown readme files into few sentences. Get short description for any repository easily.
+
+[![licence](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/bennymeg/ReadmeSummarizer/blob/master/LICENSE)
+[![npm version](https://img.shields.io/npm/v/readme-summarizer.svg)](https://www.npmjs.com/package/readme-summarizer)
+
+
+## Installation
+install...`;
 
 module.exports.expectedShortDescription = expectedShortDescription;
+module.exports.expectedShortTestDescription = expectedShortTestDescription;
 module.exports.expectedLongDescription = expectedLongDescription;
+module.exports.expectedLongTestDescription = expectedLongTestDescription;
 module.exports.readme = readme;
+module.exports.readmeTest = readmeTest;
 module.exports.readmeUrl = readmeUrl;
+module.exports.readmeUrlTest = readmeUrlTest;
